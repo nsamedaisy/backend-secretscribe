f61e94474d19b6e9d23732f51a9c9aec4ccd7aea
nsamedaisy <nsamedaisy@gmail.com>
2024-01-03 16:23:27 +0100

    authentication

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { NestExpressApplication } from '@nestjs/platform-express';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+//   app.enableCors(); // Enable CORS for all routes
+//   await app.listen(3000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+const PORT = 3003;
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors(); // Enable CORS for all routes
-  await app.listen(3000);
+  dotenv.config();
+  const app = await NestFactory.create(AppModule);
+  var cors = require('cors')
+  app.use(cors())
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(PORT).then(() => console.log(`\nserver running on port ${PORT}`));
 }
 bootstrap();
